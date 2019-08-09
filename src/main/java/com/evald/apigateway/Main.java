b32aab45e500d9d27bdc449a9197a33518c4ca6d
sindi12 <45913632+sindi12@users.noreply.github.com>
2019-08-09 03:30:14 +0200

    Update Main.java

--- src/main/java/com/evald/apigateway/Main.java
+++ src/main/java/com/evald/apigateway/Main.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Main {
 	 @RequestMapping("/")
 	    public String taleas(){
-	        return "Hello Taleas from Evald!";
+	        return "Goodmorning Taleas";
 	    }
 }
